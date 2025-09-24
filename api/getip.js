c664cd21acf9f76399d6ac39ecb851a0569b40b1
flynn.wang@fanruan.com <flynn.wang@fanruan.com>
2025-09-24 16:40:25 +0800

    Initial commit

---
+++ api/getip.js
@@ -0,0 +1,12 @@
+'''
+export default function handler(request, response) {
+  // Vercel 会将客户端的 IP 地址放在 'x-forwarded-for' 请求头中
+  const ip = request.headers['x-forwarded-for'];
+  
+  // 设置响应头为 JSON
+  response.setHeader('Content-Type', 'application/json');
+  
+  // 返回 IP 地址
+  response.status(200).json({ ip: ip });
+}
+'''
