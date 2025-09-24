065bbc173686197cfcb92fbd13ad21fa49d5eb14
flynn.wang@fanruan.com <flynn.wang@fanruan.com>
2025-09-24 17:01:12 +0800

    feat: add debug mode via query parameter

--- api/getip.js
+++ api/getip.js
@@ -1,11 +1,18 @@
 export default function handler(request, response) {
-  // Vercel 会将客户端的 IP 地址放在 'x-forwarded-for' 请求头中
-  const forwarded = request.headers['x-forwarded-for'];
-  const ip = forwarded ? forwarded.split(',')[0].trim() : request.socket.remoteAddress;
-  
-  // 设置响应头为 JSON
+  // Parse URL to check for debug query parameter
+  // A base URL is required to construct the URL object, but it's not used.
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  const isDebug = url.searchParams.get('debug') === 'true';
+
   response.setHeader('Content-Type', 'application/json');
-  
-  // 返回 IP 地址
-  response.status(200).json({ ip: ip });
+
+  if (isDebug) {
+    // In debug mode, return all headers
+    response.status(200).json(request.headers);
+  } else {
+    // In normal mode, return the client IP
+    const forwarded = request.headers['x-forwarded-for'];
+    const ip = forwarded ? forwarded.split(',')[0].trim() : request.socket.remoteAddress;
+    response.status(200).json({ ip: ip });
+  }
 }
